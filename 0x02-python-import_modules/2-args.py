30e785f71fb8b27ceb6abd9fec8c30aa5d737a13
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-06-08 09:45:30 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
         print("0 argument.")
     else:
         if total == 2:
-            print("{} argument:".format(total - 1))
+            print("{:d} argument:".format(total - 1))
         else:
-            print("{} argument:".format(total - 1))
+            print(":d{} argument:".format(total - 1))
             for i in range(1, total):
-                print("{}: {}".format(i, sys.argv[i]))
+                print("{:d}: {}".format(i, sys.argv[i]))
