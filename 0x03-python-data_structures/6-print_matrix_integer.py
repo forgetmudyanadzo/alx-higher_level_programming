85afee78ed22bb368d1be000a2becb936f7a153e
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-06-10 07:26:25 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#6-print_matrix_integer.py
+
+def print_matrix_integer(matrix=[[]]):
+    for sublist in matrix:
+        for i in range(len(sublist)):
+            print("{:d}".format(sublist[i]), end="")
+
+            if i is not len(sublist) - 1:
+                print(" ", end="")
+        print("")
