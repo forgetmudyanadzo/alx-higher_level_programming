d2d4fc5eb0813cf4f22fed093d414e4301ed0190
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-06-10 07:52:19 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+#9-max_integer.py
+
+def max_integer(my_list=[]):
+    if my_list is None or len(my_list) is 0:
+        return None
+
+    max = my_list[0]
+    for i in range(len(my_list)):
+        if max < my_list[i]:
+            max = my_list[i]
+            return max
