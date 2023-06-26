4686297513e799ff969046e122787f0508f9d038
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-06-26 10:40:01 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,12 +2,12 @@
 # 0-safe_print_list.py
 
 def safe_print_list(my_list=[], x=0):
-    n = 0
-    for i in range(x):
-        try:
-            print('{}'.format(my_list[i]), end="")
-            n += 1
-        except IndexError:
-            break
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]), end="")
+    except IndexError:
+        return i
+    else:
+        return x
+    finally:
         print("")
-        return (n)
