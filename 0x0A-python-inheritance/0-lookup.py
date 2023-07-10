f92086bc06445548e399c78af9be0f5fe992f226
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-07-10 12:24:44 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+
+"""
+    This module returns the list of available attributes
+    and methods of an object
+"""
+
+
 def lookup(obj):
-    return (dir(obj))
+    """This functions looks out for all attributes and methods of an object"""
+    return dir(obj)
