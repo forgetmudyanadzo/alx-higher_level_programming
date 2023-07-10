a117162869dfb425ec5c5b9d0db6a6d454895836
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-07-10 13:08:41 -0700

    11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+
+"""Rectangle subclass Square"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """Represent square"""
+
     def __init__(self, size):
+        """Initialize new square
+        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-
-    def __str__(self):
-        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
