2df2117ff7afd4939c4a902f595e47f5cce67871
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-10-01 01:41:03 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,14 @@
 - fetches https://alx-intranet.hbtn.io/status
 - uses urlib package
 """
-import sys
-import requests
-from requests.auth import HTTPBasicAuth
 
 
-if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
