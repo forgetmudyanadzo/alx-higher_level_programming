f20672460d1120596cadced5b0916a92efb180ac
forgetmudyanadzo <forgetmudyanadzo87@gmail.com>
2023-10-01 02:01:08 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+A Python script that fetches URL with requests package
+"""
+import requests
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
